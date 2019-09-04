7c87dd52ce9618b74dacd7caab436d082a64f799
Seungje Jun <ninanung@naver.com>
2019-09-04 11:34:55 +0900

    webpack sass setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     // enntry file
-    entry: ['@babel/polyfill', './src/js/main.js'],
+    entry: ['@babel/polyfill', './src/js/main.js', './src/sass/main.scss'],
     // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
     output: {
         path: path.resolve(__dirname, 'dist/js'),
@@ -23,7 +23,16 @@ module.exports = {
                         plugins: ['@babel/plugin-proposal-class-properties']
                     }
                 }
-            }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
         ]
     },
     devtool: 'source-map',
