461010feef1178465c7c9baa827d75bd68d62183
Seungje Jun <ninanung@naver.com>
2019-09-06 18:06:10 +0900

    webpack settings for react

---
+++ babel.webpack.react/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+    return (
+        <h1>Hello React + Babel + Webpack</h1>
+    )
+};
+
+ReactDom.render(<App/>, document.querySelector('#root'));
